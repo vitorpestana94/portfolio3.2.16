be24f9acd7399556fa6c64fbe0918a9d24cc980f
Vitorpestana94 <vitorpestanatr@gmail.com>
2023-01-16 19:13:03 -0300

    habilita inscricao

--- eventex/core/tests.py
+++ eventex/core/tests.py
@@ -2,9 +2,15 @@ from django.test import TestCase
 
 # Create your tests here.
 class HomeTest(TestCase):
+    
     def setUp(self):
         self.response = self.client.get('/')
+    
     def test_get(self):
         self.assertEqual(200, self.response.status_code)
+    
     def test_template(self):
         self.assertTemplateUsed(self.response, 'index.html')
+    
+    def test_subscription_link(self):
+        self.assertContains(self.response, 'href="/inscricao/"')
