fbafc832e45d7ad9f879fe217008612abc600277
Vitorpestana94 <vitorpestanatr@gmail.com>
2023-01-17 17:23:23 -0300

    enviando email com SendGrid

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -12,8 +12,8 @@ def subscribe(request):
         if form.is_valid():
             
             body =  render_to_string('subscriptions/subscription_email.txt', form.cleaned_data)
-            mail.send_mail('confirmação de inscrição', body, 'contato@eventex.com.br', 
-                            ['contato@eventex.com.br', form.cleaned_data['email']])
+            mail.send_mail('confirmação de inscrição', body, 'vitorpestanatr@gmail.com', 
+                            ['vitorpestanatr@gmail.com', form.cleaned_data['email']])
             
             messages.success(request, 'Inscrição realizada com sucesso!')
             
