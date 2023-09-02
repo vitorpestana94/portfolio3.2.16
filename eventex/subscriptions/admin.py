2dac19e1b1033f97608f5ad4909b78932ccfdd5e
Vitor Luiz Pestana Pereira Menezes <vitorpestanatr@gmail.com>
2023-09-01 21:45:04 -0300

    Update admin.py

--- eventex/subscriptions/admin.py
+++ eventex/subscriptions/admin.py
@@ -4,10 +4,10 @@ from django.utils.timezone import now
 
 
 class SubscriptionModelAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'CPF', 'email', 'telefone', 'created_at', 
+    list_display = ('nome', 'email', 'telefone', 'created_at', 
                     'subscribed_today', 'paid')
     date_hierarchy = 'created_at'
-    search_fields = ('nome', 'email', 'CPF', 'telefone', 'created_at')
+    search_fields = ('nome', 'email','telefone', 'created_at')
     list_filter = ('paid', 'created_at')
     actions = ['mark_as_paid']
 
